9652c33bdca3e7a2fbcc697a69f1d95043c52e66
Lance Pollard <lp@elk.fm>
2026-06-20 01:49:48 -0700

    save

--- host/web-blog/app.js
+++ host/web-blog/app.js
@@ -1,4 +1,4 @@
-// ../../../../../../../../../private/var/folders/8x/z26qdb3x465gmtqr6z4x8hgm0000gn/T/seed-blog-XJJjyD/app.ts
+// ../../../../../../../../../private/var/folders/8x/z26qdb3x465gmtqr6z4x8hgm0000gn/T/seed-blog-P7r5uV/app.ts
 var page = document;
 function createElement(tag) {
   const made = page.createElement(tag, { form: "none" });
@@ -57,5 +57,5 @@ function makeApp(host) {
   mount(host, () => root);
 }
 
-// ../../../../../../../../../private/var/folders/8x/z26qdb3x465gmtqr6z4x8hgm0000gn/T/seed-blog-XJJjyD/entry.ts
+// ../../../../../../../../../private/var/folders/8x/z26qdb3x465gmtqr6z4x8hgm0000gn/T/seed-blog-P7r5uV/entry.ts
 makeApp({ handle: document.body });
